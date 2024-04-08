89196f4eaa5413421d8f50c5865f6d140296fd04
yasliks <perejaslov9200@gmail.com>
2024-04-08 16:45:14 +0300

    fix: fields in PhotoItem, Urls, User are made constants; fix docs

--- lib/api/data/user.dart
+++ lib/api/data/user.dart
@@ -2,16 +2,22 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'user.g.dart';
 
+/// {@template user.class}
+/// DTO for the user who posted the photo
+/// {@endtemplate}
 @JsonSerializable(explicitToJson: true)
 class User {
-  String username;
+  /// name of user
+  final String username;
 
-  User({required this.username});
+  /// {@macro user.class}
+  const User({
+    required this.username,
+  });
 
-  // From Json
-  factory User.fromJson(Map<String, dynamic> json) =>
-      _$UserFromJson(json);
+  /// Get DTO object from json.
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
-  // ToJson
+  /// Convert a DTO object to json.
   Map<String, dynamic> toJson() => _$UserToJson(this);
 }
--- lib/api/service/photo/photo_api.dart
+++ lib/api/service/photo/photo_api.dart
@@ -4,13 +4,13 @@ import 'package:retrofit/retrofit.dart';
 
 part 'photo_api.g.dart';
 
-/// API for ip.
+/// API for photo API Unsplash.
 @RestApi()
 abstract class PhotoApi {
   /// API creation factory using [Dio].
   factory PhotoApi(Dio dio, {String baseUrl}) = _PhotoApi;
 
-  /// Request to get the current IP address.
+  /// Request to get the current list of photos
   @GET('')
   Future<List<PhotoItem>> getPhoto([@Query('format') String format = 'json']);
 }
