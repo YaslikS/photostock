a48ec29de5755235c04be0517b5a2a982485c77d
yasliks <perejaslov9200@gmail.com>
2024-04-09 10:32:00 +0300

    fix: accessKey and url are provided in appconfig

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -10,6 +10,10 @@ class AppConfig {
   /// Proxy url.
   final String? proxyUrl;
 
+  /// access key for
+  final String accessKey =
+      '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043';
+
   /// {@macro app_config.class}
   const AppConfig({
     required this.url,
--- lib/features/photostock_list/di/photo_list_scope.dart
+++ lib/features/photostock_list/di/photo_list_scope.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/api/service/ip/ip_api.dart';
 import 'package:flutter_template/api/service/photo/photo_api.dart';
 import 'package:flutter_template/common/utils/disposable_object/disposable_object.dart';
 import 'package:flutter_template/common/utils/disposable_object/i_disposable_object.dart';
 import 'package:flutter_template/features/app/di/app_scope.dart';
-import 'package:flutter_template/features/feature_example/data/converters/ip_converter.dart';
-import 'package:flutter_template/features/feature_example/data/repositories/feature_example_repository.dart';
-import 'package:flutter_template/features/feature_example/domain/repositories/i_feature_example_repository.dart';
 import 'package:flutter_template/features/photostock_list/data/converters/photo_converter.dart';
 import 'package:flutter_template/features/photostock_list/data/repositories/photos_repository.dart';
 import 'package:flutter_template/features/photostock_list/domain/repositories/i_photos_repository.dart';
@@ -22,13 +18,12 @@ final class PhotoListScope extends DisposableObject implements IPhotoListScope {
   /// Factory constructor for [IPhotoListScope].
   factory PhotoListScope.create(BuildContext context) {
     final appScope = context.read<IAppScope>();
-    const photoUrl = 'https://api.unsplash.com/photos/?client_id=';
-    const accessKey =
-        '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043';
+    final photoUrl = appScope.appConfig.url.value;
+    final accessKey = appScope.appConfig.accessKey;
 
     return PhotoListScope(
       PhotosRepository(
-        photoApi: PhotoApi(appScope.dio, baseUrl: photoUrl+accessKey),
+        photoApi: PhotoApi(appScope.dio, baseUrl: photoUrl + accessKey),  // TODO: YaslikS - remove accessKey from url
         photoConverter: const PhotoConverter(),
       ),
     );
