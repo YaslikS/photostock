4dba06bc356de50d938dabe61d438e52f3b2effb
yasliks <perejaslov9200@gmail.com>
2024-04-15 13:03:09 +0300

    feat: attempt to take out a Custom ScrollView from a PhotosListWidget

--- lib/features/photostock_list/presentation/photo_list_screen.dart
+++ lib/features/photostock_list/presentation/photo_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:elementary/elementary.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_template/features/common/utils/sizes/app_sizes.dart';
 import 'package:flutter_template/features/photostock_list/presentation/photo_list_state.dart';
 import 'package:flutter_template/features/photostock_list/presentation/photo_list_wm.dart';
 import 'package:flutter_template/features/photostock_list/presentation/widgets/photos_list_widget.dart';
@@ -21,25 +22,51 @@ class PhotoListScreen extends ElementaryWidget<IPhotoListWM> {
 
     return Scaffold(
       backgroundColor: wm.colorScheme.background,
-      body: ValueListenableBuilder<PhotoListState>(
-        valueListenable: wm.state,
-        builder: (_, state, __) => switch (state) {
-          PhotoListStateInitial _ => const Center(
-              child: SizedBox.shrink(),
-            ),
-          PhotoListStateLoading _ => const Center(
-              child: CupertinoActivityIndicator(),
-            ),
-          PhotoListStateLoaded(:final photoEntity) => PhotosListWidget(
-              photosList: photoEntity.photos,
-              listNeedsUpdate: wm.listNeedsUpdate,
-            ),
-          PhotoListStateError _ => Center(
-              child: Text(
-                wm.l10n.photoListFailedLoadListPhotoMessage,
+      body: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(
+            backgroundColor: wm.colorScheme.background,
+            largeTitle: Text(wm.l10n.photoListTitle),
+            border: Border(
+              bottom: BorderSide(
+                color: wm.colorScheme.background,
               ),
             ),
-        },
+          ),
+          SliverToBoxAdapter(
+            child: ValueListenableBuilder<PhotoListState>(
+              valueListenable: wm.state,
+              builder: (_, state, __) => switch (state) {
+                PhotoListStateInitial _ => const Center(
+                    child: SizedBox.shrink(),
+                  ),
+                PhotoListStateLoading _ => const Center(
+                    child: CupertinoActivityIndicator(),
+                  ),
+                PhotoListStateLoaded(:final photoEntity) => PhotosListWidget(
+                    photosList: photoEntity.photos,
+                    listNeedsUpdate: wm.listNeedsUpdate,
+                  ),
+                PhotoListStateError _ => Center(
+                    child: Text(
+                      wm.l10n.photoListFailedLoadListPhotoMessage,
+                    ),
+                  ),
+              },
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: AppSizes.double32,
+                top: AppSizes.double20,
+              ),
+              child: Center(
+                child: CupertinoActivityIndicator(),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/features/photostock_list/presentation/widgets/photos_list_widget.dart
+++ lib/features/photostock_list/presentation/widgets/photos_list_widget.dart
@@ -2,11 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/api/data/photo_item.dart';
 import 'package:flutter_template/features/common/utils/sizes/app_constants.dart';
-import 'package:flutter_template/features/common/utils/sizes/app_sizes.dart';
-import 'package:flutter_template/features/photostock_list/presentation/photo_list_wm.dart';
 import 'package:flutter_template/features/photostock_list/presentation/widgets/photo_list_item_widget.dart';
-import 'package:flutter_template/l10n/app_localizations_x.dart';
-import 'package:flutter_template/uikit/colors/app_color_scheme.dart';
 
 /// {@template photos_list_widget.class}
 /// PhotoListWidget.
@@ -27,8 +23,6 @@ class PhotosListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = AppColorScheme.of(context);
-    final l10n = context.l10n;
     return NotificationListener<ScrollNotification>(
       onNotification: (notification) {
         if (notification is ScrollEndNotification &&
@@ -37,38 +31,15 @@ class PhotosListWidget extends StatelessWidget {
         }
         return false;
       },
-      child: CustomScrollView(
-        slivers: [
-          CupertinoSliverNavigationBar(
-            backgroundColor: colorScheme.background,
-            largeTitle: Text(l10n.photoListTitle),
-            border: Border(
-              bottom: BorderSide(
-                color: colorScheme.background,
-              ),
-            ),
-          ),
-          SliverGrid.builder(
-            itemCount: photosList.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: AppConstants.int2,
-            ),
-            itemBuilder: (context, i) {
-              return PhotoListItemWidget(photoItem: photosList[i]);
-            },
-          ),
-          const SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.only(
-                bottom: AppSizes.double32,
-                top: AppSizes.double20,
-              ),
-              child: Center(
-                child: CupertinoActivityIndicator(),
-              ),
-            ),
-          ),
-        ],
+      child: GridView.builder(
+        shrinkWrap: true,
+        itemCount: photosList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: AppConstants.int2,
+        ),
+        itemBuilder: (context, i) {
+          return PhotoListItemWidget(photoItem: photosList[i]);
+        },
       ),
     );
   }
