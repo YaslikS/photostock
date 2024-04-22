81fbb4ee2568cb9f37f194ce6643360ce4fbbd09
yasliks <perejaslov9200@gmail.com>
2024-04-22 11:13:08 +0300

    fix: refactoring

--- lib/features/photostock_list/presentation/photo_list_wm.dart
+++ lib/features/photostock_list/presentation/photo_list_wm.dart
@@ -5,10 +5,10 @@ import 'package:flutter_template/common/mixin/localization_mixin.dart';
 import 'package:flutter_template/features/app/di/app_scope.dart';
 import 'package:flutter_template/features/common/utils/mixin/theme_wm_mixin.dart';
 import 'package:flutter_template/features/photostock_list/di/photo_list_scope.dart';
-import 'package:flutter_template/features/photostock_list/presentation/uploaded_list_state.dart';
 import 'package:flutter_template/features/photostock_list/presentation/photo_list_model.dart';
 import 'package:flutter_template/features/photostock_list/presentation/photo_list_screen.dart';
 import 'package:flutter_template/features/photostock_list/presentation/screen_list_state.dart';
+import 'package:flutter_template/features/photostock_list/presentation/uploaded_list_state.dart';
 import 'package:provider/provider.dart';
 
 /// DI factory for [PhotoListWM].
@@ -95,7 +95,7 @@ final class PhotoListWM extends WidgetModel<PhotoListScreen, PhotoListModel>
 
   void _observerStateNewList() {
     stateNewList.addListener(() {
-      if (stateNewList.value is NewListStateError) {
+      if (stateNewList.value is UploadedListStateError) {
         _showErrorAppSnackBar();
       }
     });
